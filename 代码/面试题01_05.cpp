565d5148970890b0ed774964ae3b2667b2d8226d
summer666max <73483986+summer666max@users.noreply.github.com>
2022-05-13 10:37:23 +0800

    Create 面试题01_05.cpp

---
+++ 代码/面试题01_05.cpp
@@ -0,0 +1,99 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    bool oneEditAway(string first, string second) {
+        if (first == second) {
+            return true;
+        }
+        int len1 = first.size();
+        int len2 = second.size();
+        int i, j;
+        int count;
+        if (len1 == len2)            //替换
+        {
+            i = 0;
+            count = 0;
+            while (i < len1)
+            {
+                if (first[i] != second[i])
+                {
+                    ++count;
+                    if (count > 1)
+                    {
+                        return false;
+                    }
+                }
+                ++i;
+            }
+        }
+        else if (len1 == len2 + 1)           //删除
+        {
+            i = 0;
+            j = 0;
+            count = 0;
+            while (i < len1 && j < len2)
+            {
+                if (first[i] != second[j])
+                {
+                    ++i;
+                    ++count;
+                    if (count > 1)
+                    {
+                        return false;
+                    }
+                    continue;
+                }
+                else
+                {
+                    ++i;
+                    ++j;
+                }
+            }
+
+        }
+        else if (len1 == len2 - 1)                        //插入
+        {
+            i = 0;
+            j = 0;
+            count = 0;
+            while (i < len1 && j < len2)
+            {
+                if (first[i] != second[j])
+                {
+                    ++j;
+                    ++count;
+                    if (count > 1)
+                    {
+                        return false;
+                    }
+                    continue;
+                }
+                else
+                {
+                    ++i;
+                    ++j;
+                }
+            }
+        }
+        else                                //一次操作无法完成
+        {
+            return false;
+        }
+        return true;
+
+    }
+};
+
+int main()
+{
+    //测试
+    //string a = "abc";
+    //string b = "abec";
+    //Solution solution;
+    //cout << solution.oneEditAway(a, b) << endl;
+
+    return 0;
+}
