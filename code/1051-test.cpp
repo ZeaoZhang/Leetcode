780fefb5cc2c2d1c9c1583f1860b251f5db3a4b7
adversityh <2936995453@qq.com>
2022-06-13 21:05:41 +0800

    1051 高度检查器

---
+++ code/1051-test.cpp
@@ -0,0 +1,13 @@
+#include"./includes/1051.hpp"
+
+int main()
+{
+    vector<int> heights ={1, 1, 4, 2, 1, 3};
+    //expected = {1， 1， 1， 2， 3， 4} 输出3
+    Solution solution;
+    if(solution.heightChecker(heights) == 3)
+    {
+        return 1;
+    }
+    return 0;
+}
---
+++ code/includes/1051.hpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> heights_image(heights);
+        sort(heights_image.begin(), heights_image.end());
+        int count = 0;
+        int n = heights.size();
+        for(int i = 0; i < n; ++i)
+        {
+            if(heights[i] != heights_image[i])
+            {
+                ++count;
+            }
+        }
+        return count;
+    }
+};
+
