8d1ae4b8b92a6ce3faeee92fb9e02c3375df283d
adversityh <2936995453@qq.com>
2022-06-23 10:55:26 +0800

    改正

--- dockerfile
+++ dockerfile
@@ -6,7 +6,7 @@ ADD . /leetcode
 # 设置容器启动后的默认运行目录
 WORKDIR /leetcode/code
 
-RUN apt-get update && apt-get -y install g++ && apt-get -y install make
+RUN apt-get update && apt-get -y install g++ make 
 
 
 
--- help.sh
+++ help.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-if [[ -n $(sudo docker ps -qf "name=leetcode") ]]
+read -p "please input the name of the container:" containername
+
+#若此容器已存在但是还没有开始
+if [[ -z $(sudo docker ps -qf "name=$containername") ]] && [[ -n $(sudo docker ps -qaf "name=$containername") ]] 
 then 
-	:
-elif [[ -n $(sudo docker ps -qaf "name=leetcode") ]]
+	sudo docker start $containername
+#若此容器不存在
+elif [[ -z $(sudo docker ps -qaf "name=$containername") ]]
 then
-	sudo docker start leetcode
-else
-	sudo docker image rm -f leetcode:v1
-	sudo docker build -t leetcode:v1 .
-	sudo docker run -dit -p 8080:8080 --name leetcode leetcode:v1
+	read -p "please input the name of the image: " imagename
+	read -p "please input the edition of the image: " edition
+	#若此镜像不存在，则先创建镜像
+	if [[ -z $(sudo docker images -q $imagename:$edition) ]]
+	then
+		sudo docker build -t $imagename:$edition .
+	fi
+	sudo docker run -dit --name $containername $imagename:$edition
 fi
-id=$(sudo docker ps -aqf "name=leetcode")
+#得到容器id
+id=$(sudo docker ps -aqf "name=$containername")
 
 while :
 do
@@ -19,12 +27,11 @@ echo -e "Please select an action: \n \
 	0 Show all source files\n \
 	1 Compile all source files\n \
 	2 Show all executable files\n \
-	3 test all executable files\n \
-	4 exit help\n \
-	5 clean all executable files\n\
-	6 download all executable files\n\
-	7 stop the container\n\
-	8 end and delete the container"
+	3 Test all executable files\n \
+	4 Clean all executable files\n\
+	5 Download all executable files\n\
+	6 Stop the container\n\
+	7 End and delete the container"
 
 read -p "please input number: " number
 case $number in
@@ -41,24 +48,29 @@ case $number in
 		sudo docker exec -it $id /bin/bash -c 'make test'
 		;;
 	4)
-		exit
+		sudo docker exec -it $id /bin/bash -c 'make clean'
+		echo "Finish"
 		;;
 	5)
-		sudo docker exec -it $id /bin/bash -c 'make clean'
-		;;
-	6)
-		mkdir ./download
+		#若文件夹不存在则创建
+		if [ ! -d "./download" ]
+		then
+			mkdir ./download
+		fi
 		for file in $(sudo docker exec -it $id /bin/bash -c 'ls | grep .*-test$')
 		do
+		#去除file首尾空格得到文件名
 		s=`echo $file | sed s/[[:space:]]//g`
+		#下载文件
 		sudo docker cp $id:/leetcode/code/$s ./download/
 		done
+		echo "Finish"
 		;;
-	7)
+	6)
 		sudo docker container stop $id
 		exit
 		;;
-	8)
+	7)
 		sudo docker container rm -f $id
 		exit
 		;;
